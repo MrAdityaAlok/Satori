5cf83d649a5e3083858212c67a25efb774d8e0c5
Aditya Alok <dev.aditya.alok@gmail.com>
2026-01-20 17:55:43 +0530

    feat: add font and cursor installation scripts
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,10 +9,27 @@ mkdir -p /var/roothome
 mkdir -p /usr/local/sbin
 
 # Needed for build:
-dnf5 -y install dnf5-plugins rsync
+dnf5 -y install dnf5-plugins rsync fontconfig
 
 # https://bugzilla.redhat.com/show_bug.cgi?id=2332429
 dnf5 -y swap --repo='fedora' OpenCL-ICD-Loader ocl-icd
+
+install_cursor_themes() {
+	echo "INFO: Installing cursor themes ..."
+
+	mkdir -p /usr/share/icons/BreezeX-RosePine-Linux/
+
+	curl -fLsS --retry 5 --create-dirs \
+		https://github.com/rose-pine/cursor/releases/latest/download/BreezeX-RosePine-Linux.tar.xz \
+		-o /tmp/cursor/pine.tar.xz
+
+	tar -xf /tmp/cursor/pine.tar.xz -C /usr/share/icons/BreezeX-RosePine-Linux/ --strip-components=1
+
+	echo "INFO: Installed cursor themes."
+}
+
+/ctx/utils/install-nerd-font "Iosevka"
+install_cursor_themes
 
 /ctx/common/packages.sh
 /ctx/common/flatpaks.sh
--- build_files/hyprland/build.sh
+++ build_files/hyprland/build.sh
@@ -2,6 +2,23 @@
 
 set -oeux pipefail
 
+install_hyprcursor_themes() {
+	echo "INFO: Installing hyprcursor themes ..."
+
+	mkdir -p /usr/share/icons/rose-pine-hyprcursor/
+
+	curl -fLsS --retry 5 --create-dirs \
+		https://github.com/ndom91/rose-pine-hyprcursor/releases/download/v0.3.2/rose-pine-cursor-hyprcursor_0.3.2.tar.gz \
+		-o /tmp/cursor/hyprcursor.tar.xz
+
+	tar -xf /tmp/cursor/pine.tar.xz -C /usr/share/icons/rose-pine-hyprcursor/
+
+	echo "INFO: Installed hyprcursor themes."
+}
+
+/ctx/utils/install-nerd-font "CodeNewRoman" # For waybar. Iosevka is already installed in common.
+install_hyprcursor_themes
+
 /ctx/hyprland/packages.sh
 /ctx/hyprland/systemd.sh
 
