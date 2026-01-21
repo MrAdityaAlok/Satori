b84b01fb1d87a9ba6e5ac0d062bdd7d064b8af30
Aditya Alok <dev.aditya.alok@gmail.com>
2026-01-22 00:15:21 +0530

    feat: enable gpg-agent{,-ssh}.socket globally
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- build_files/common/systemd.sh
+++ build_files/common/systemd.sh
@@ -2,6 +2,6 @@
 
 set -ouex pipefail
 
-systemctl enable podman.socket
 systemctl enable --global foot-server.socket
 systemctl enable dconf-update.service
+systemctl enable --global gpg-agent{,-ssh}.socket
